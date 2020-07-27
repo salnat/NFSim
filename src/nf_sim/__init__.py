83aea4895b20b8ff8a94e0abdd70a2ebdf5e4d62
salnat <48283086+salnat@users.noreply.github.com>
2020-07-27 13:00:39 +0300

    Initial Commit

---
+++ src/nf_sim/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__title__ = "NFSim"
+__description__ = "Neurofeedback simulation"
+__url__ = <GITHUB LINK>
+__doc__ = __description__ + " <" + __url__ + ">"
+__author__ = "Saleh Natour"
+__email__ = "salnat145@gmail.com"
+__license__ = "MIT"
